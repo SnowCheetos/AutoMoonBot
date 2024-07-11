34bf491dda5c7b633bd1ae91388e8c974b1fa042
zhihaos2 <cassfunen@yahoo.com>
2024-07-11 17:40:27 -0500

    added mut test cases

--- tests/backend/data/element_tests.py
+++ tests/backend/data/element_tests.py
@@ -16,10 +16,13 @@ def test_basics():
 
     element = Element(var=1)
     assert element.var == 1, "Incorrect instantiation of element attr var"
-    
+
     with pytest.raises(Exception) as e:
         _ = element.x
-    assert isinstance(e.value, AttributeError), "Failed to check for attribute existence"
+    assert isinstance(
+        e.value, AttributeError
+    ), "Failed to check for attribute existence"
+
 
 def test_attr():
     element = Element(on_error="omit")
@@ -27,15 +30,20 @@ def test_attr():
 
     class Example(Element):
         def __init__(self, on_error, **kwargs):
-            super().__init__(on_error, **kwargs)
-        
+            super().__init__(
+                mutable=True,
+                on_error=on_error,
+                **kwargs,
+            )
+
         def get_attr(self):
             return {"var": self.var}
-    
+
     element = Example(on_error="omit", var=1)
     assert element.attr() == {"var": 1}, "Element failed to use `get_attr(...)`"
 
     # TODO Lambda scripts
+
 
 def test_tensor():
     element = Element(on_error="omit")
@@ -43,15 +51,20 @@ def test_tensor():
 
     class Example(Element):
         def __init__(self, on_error, **kwargs):
-            super().__init__(on_error, **kwargs)
-        
+            super().__init__(
+                mutable=True,
+                on_error=on_error,
+                **kwargs,
+            )
+
         def get_tensor(self):
             return torch.tensor(self.var)
-    
+
     element = Example(on_error="omit", var=1)
     assert element.tensor().item() == 1, "Element failed to use `get_tensor(...)`"
 
     # TODO Lambda scripts
+
 
 def test_update():
     element = Element(on_error="omit")
@@ -59,13 +72,35 @@ def test_update():
 
     class Example(Element):
         def __init__(self, on_error, **kwargs):
-            super().__init__(on_error, **kwargs)
-        
+            super().__init__(
+                mutable=True,
+                on_error=on_error,
+                **kwargs,
+            )
+
         def get_update(self, **kwargs):
             return kwargs
-    
+
     element = Example(on_error="omit", var=1)
     assert element.update(var=1) == True, "Element failed to perform update"
     assert element.var == 1, "Element updated the wrong value"
+
+    class Example(Element):
+        def __init__(self, on_error, **kwargs):
+            super().__init__(
+                mutable=False,
+                on_error=on_error,
+                **kwargs,
+            )
+
+        def get_update(self, **kwargs):
+            return kwargs
+
+    element = Example(on_error="raise", var=1)
+    with pytest.raises(Exception) as e:
+        element.update(var=2)
+    assert isinstance(
+        e.value, AttributeError
+    ), "Failed to check for attribute existence"
 
     # TODO Lambda scripts
