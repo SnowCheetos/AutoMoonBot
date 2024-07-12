d0159ed173f45b5fd082f01f3661d88c495a4223
zhihaos2 <cassfunen@yahoo.com>
2024-07-12 18:39:10 -0500

    safer streamer

--- backend/data/streamer.py
+++ backend/data/streamer.py
@@ -8,33 +8,49 @@ from backend.data import DBInterface
 
 
 class Streamer:
+    done_policies = {"omit", "raise"}
+
     def __init__(
         self,
         queue_size: int,
+        done: str = "omit",
         data: Iterable | None = None,
         **kwargs,
     ) -> None:
         self._running = False
         self._data = iter(data) if data is not None else None
         self._queue = Queue(maxsize=queue_size)
-        self._thread = threading.Thread(
-            target=self._fetch_loop,
-            kwargs=kwargs,
-        )
+        self._lock = threading.Lock()
+        self._thread = None
+        self._kwargs = kwargs
+        assert (
+            done in self.__class__.done_policies
+        ), f"invalid done policy, must be one of {self.__class__.done_policies}"
+        self._done = done
 
     @property
     def running(self) -> bool:
-        return self._running
+        with self._lock:
+            return self._running
 
     def start(self) -> None:
-        self._running = True
-        self._thread.start()
+        with self._lock:
+            if self._running:
+                return
+            self._running = True
+            self._thread = threading.Thread(
+                target=self._fetch_loop, kwargs=self._kwargs
+            )
+            self._thread.start()
 
     def stop(self) -> None:
-        self._running = False
+        with self._lock:
+            if not self._running:
+                return
+            self._running = False
         self._thread.join()
 
-    def prefill(self, **kwargs) -> None:
+    def prefill(self, **_) -> None:
         """
         Override this method for specific types of streamers
         """
@@ -47,7 +63,7 @@ class Streamer:
                 return
             self._store(data)
 
-    def get_data(self, sleep: int=1) -> Any:
+    def get_data(self, sleep: int = 1) -> Any | None:
         """
         Override this method for specific types of streamers
         """
@@ -66,24 +82,40 @@ class Streamer:
         try:
             self._queue.put_nowait(data)
         except Full:
-            disgard = self._queue.get_nowait()
-            self._queue.put(data)
-            del disgard
+            try:
+                discarded = self._queue.get_nowait()
+                del discarded
+                self._queue.put(data)
+            except Empty:
+                return
 
     def _fetch_loop(self, **kwargs) -> None:
         self.prefill(**kwargs)
-        while self._running:
+        while self.running:
             data = self.get_data(**kwargs)
             self._store(data)
 
     def __iter__(self) -> Iterable:
         return self
 
-    def __next__(self) -> Any:
+    def __next__(self) -> Any | None:
         try:
             return self._queue.get_nowait()
         except Empty:
-            return None
+            if self._done == "omit":
+                return None
+            elif self._done == "raise":
+                raise StopIteration
+
+    def __del__(self) -> None:
+        self.stop()
+
+    def __enter__(self) -> Any:
+        self.start()
+        return self
+
+    def __exit__(self, *_) -> None:
+        self.stop()
 
 
 class DBStreamer(Streamer, DBInterface):
