f8edb919c08872dfe9ccae64f8dd254efbbd9a85
SnowCheetos <cassfunen@yahoo.com>
2024-07-18 15:20:55 -0500

    basic edge setup

--- moonrs/src/edges/common.rs
+++ moonrs/src/edges/common.rs
@@ -8,10 +8,10 @@ pub mod static_relations {
 
         #[derive(Debug, Clone)]
         pub struct Mentioned {
-            pub(in crate::edges) source: String,
-            pub(in crate::edges) target: String,
-            pub(in crate::edges) src_id: String,
-            pub(in crate::edges) tgt_id: String,
+            pub(in crate::edges) src_ty: String,
+            pub(in crate::edges) tgt_ty: String,
+            pub(in crate::edges) src_nm: String,
+            pub(in crate::edges) tgt_nm: String,
             pub(in crate::edges) relevance: f64,
             pub(in crate::edges) sentiment: f64,
         }
@@ -25,16 +25,16 @@ pub mod static_relations {
 
         impl Mentioned {
             pub fn new(
-                source: &Article,
-                target: &StaticEvent,
+                src_ty: &Article,
+                tgt_ty: &StaticEvent,
                 relevance: f64,
                 sentiment: f64,
             ) -> Self {
                 Mentioned {
-                    source: source.cls().to_owned(),
-                    target: target.cls().to_owned(),
-                    src_id: source.name().to_owned(),
-                    tgt_id: target.name().to_owned(),
+                    src_ty: src_ty.cls().to_owned(),
+                    tgt_ty: tgt_ty.cls().to_owned(),
+                    src_nm: src_ty.name().to_owned(),
+                    tgt_nm: tgt_ty.name().to_owned(),
                     relevance,
                     sentiment,
                 }
